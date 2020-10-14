e8da59acf63fe0981b95c5a6bb48e5462dc7f7f1
Kuba Unold <jakub@unold.pl>
2020-10-14 11:05:49 +0200

    my minor changes.

--- VRP/app.py
+++ VRP/app.py
@@ -1,6 +1,9 @@
 import csv
 
 with open('data/PL.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
     for row in spamreader:
         print(', '.join(row))
+
+
+t = [1, 2, 3]
