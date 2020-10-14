fd62db5197a71844f050621f762dd8e01a40668d
Kuba Unold <jakub@unold.pl>
2020-10-14 21:19:13 +0200

    Added generating permutation.

--- VRP/app.py
+++ VRP/app.py
@@ -1,8 +1,11 @@
 from typing import List # to use type aliasing
 import csv
+from random import randint
+from collections import Counter # to count distinct objects in a list
+import time
 
 # type alias
-Vector = List[float]
+Vector = List[int]
 
 # # This is an exapmle hot type aliasing works
 # def scale(scalar: float, vector: Vector) -> Vector:
@@ -11,8 +14,68 @@ Vector = List[float]
 # print(new_vector)
 
 
-def generatePermutation(n:int) -> Vector:
-    return [0, 2, 3, 1, 0]
+def generatePermutation(n:int, h:int, c:int) -> Vector:
+    """This function generates random permutation for given starting (and ending) city (aka HUB)
+
+    Right now it visits every city once and uses from 1 to maximum cars.
+    Some cars may have "empty" rides
+
+    Args:
+        n (int): Number of cities
+        h (int): Index of starting city (hub)
+        c (int): Number of cars
+
+    Returns:
+        perm (Vector): Generated permutation as a List[int]
+
+    Example:
+        If there are 5 cities, then indexes are 0...4.
+        generatePermutation(5, 0, 1) will produce e.g. such output:
+        [0, 3, 4, 1, 0]
+    """
+    ############################
+    # config params
+    # everyCarIsUsed = True
+    everyCityIsVisited = True
+    #############################
+
+    # counts distinct objects in a list
+    def countDistinct(arr): 
+        return len(Counter(arr).keys())
+
+    # create list only w/ hub at the beginning
+    perm = [h]
+    # print (perm)
+    # time.sleep(3)
+    visitedCities = [h]
+    noOfUsedCars = 1
+
+    #now use every car (HARDCODED)
+    if everyCityIsVisited:
+        while countDistinct(visitedCities) < n:
+            randNum = randint(0, n-1)
+            # print(f"Current rand: {randNum}, Current list: {perm}")
+            
+            # is the city a hub?
+            if randNum == h:
+                if noOfUsedCars == c:
+                    continue
+                else:
+                    perm.append(randNum)
+                    noOfUsedCars+=1
+                    continue
+            # selected city is not a hub
+            else:
+                # city not yet visited
+                if randNum not in visitedCities:
+                    perm.append(randNum)
+                    visitedCities.append(randNum)
+                # city already visited
+                else:
+                    continue
+        perm.append(h)
+    return perm
+
 
 
 def main():
@@ -22,8 +85,13 @@ def main():
         #     print(', '.join(row))
         data = list(spamreader)
         print(str(data))
+    n = 12  # number of cities
+    h = 7   # hub's id 
+    c = 4   # number of cars
+
+    permutation = generatePermutation(n, h, c)
+    print(permutation)
 
 
 if __name__ == "__main__":
-    # main()
-    print(generatePermutation(5))
+    main()
