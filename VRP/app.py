bcab7864671b944730cedce2044d81010b7a677a
Kuba Unold <jakub@unold.pl>
2020-10-14 10:43:43 +0200

    Added gitignore. Working data reading.

---
+++ VRP/app.py
@@ -0,0 +1,6 @@
+import csv
+
+with open('data/PL.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
