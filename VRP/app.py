773725d374acddfc5c2c146b647d2bac9a0f55ce
Kuba Unold <jakub@unold.pl>
2020-10-14 15:56:36 +0200

    Added type aliasing.

--- VRP/app.py
+++ VRP/app.py
@@ -1,8 +1,18 @@
+from typing import List # to use type aliasing
 import csv
 
+# type alias
+Vector = List[float]
+
+# # This is an exapmle hot type aliasing works
+# def scale(scalar: float, vector: Vector) -> Vector:
+#     return [scalar * num for num in vector]
+# new_vector = scale(2.0, [1.0, -4.2, 5.4])
+# print(new_vector)
 
 
-
+def generatePermutation(n:int) -> Vector:
+    return [0, 2, 3, 1, 0]
 
 
 def main():
@@ -13,5 +23,7 @@ def main():
         data = list(spamreader)
         print(str(data))
 
+
 if __name__ == "__main__":
-    main()
+    # main()
+    print(generatePermutation(5))
