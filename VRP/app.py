4345c9ca3f2c20e8ce994f15743579679ef5c2ca
Kacper Starościak <k.starosciak@etd.net.pl>
2020-10-14 11:04:48 +0200

    read to list

--- VRP/app.py
+++ VRP/app.py
@@ -1,6 +1,11 @@
 import csv
 
+
+
 with open('data/PL.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    # for row in spamreader:
+    #     print(', '.join(row))
+    data=list(spamreader)
+
+print(str(data))
